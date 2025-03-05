b0f357692566889da19dc19e0606c02fa3e48d2f
MidotG <shin.alexey1@gmail.com>
2025-03-05 23:11:45 +0300

    C3 - adding floor#2.
    Trying multimesh.

--- watcher.gd
+++ watcher.gd
@@ -29,7 +29,9 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit();
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
